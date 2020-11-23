6c085d6fd282bf70ddb17ab5e4f80c3bd2a822cd
unknown <thiagovcs@al.insper.edu.br>
2020-11-23 15:49:36 -0300

    pontos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
+var gWidth = window.innerWidth;
+var gHeight = window.innerHeight;
+var ratio = gWidth / gHeight;
+var borders = [40, 24] //indicate where the ball needs to move in mirror position
+
+var light = new THREE.AmbientLight(0xffffff, 0.5);
+var light1 = new THREE.PointLight(0xffffff, 0.5);
+light1.position.set(0, 5, 0);
+light1.castShadow = true;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10 );
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 var input = new Input();
 
-const renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
+//properties for casting shadow
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( .3, 1, .1);
@@ -25,13 +40,56 @@ const wall_right = new THREE.Mesh( geometry4, material2);
 const wall_down = new THREE.Mesh( geometry3, material2);
 const line = new THREE.Mesh( geometry5, material2);
 
+var loader = new THREE.FontLoader();
+var mesh;
+
+function render() {
+  if (mesh) {
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.007;
+  }
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+var placar_1 = 0;
+var placar_2 = 0;
+
+loader.load('https://cdn.rawgit.com/mrdoob/three.js/master/examples/fonts/helvetiker_regular.typeface.json', function(font) {
+  
+  var text = new THREE.TextGeometry(`${placar_1} x ${placar_2}`, {
+    font: font,
+    size: 50,
+    height: 0,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 5,
+    bevelSize: 8,
+    bevelSegments: 5
+  });
+
+  var material = new THREE.MeshLambertMaterial({
+    color: 0xF3FFE2
+  });
+  mesh = new THREE.Mesh(text, material);
+  mesh.position.set(-.75, 2.5, -.3);
+  mesh.scale.multiplyScalar(0.01)
+  mesh.castShadow = true;
+  scene.add(mesh);
+
+});
+
 player1.position.x = -4;
+player1.reciveShadow = true;
 player2.position.x = 4;
+player2.reciveShadow = true;
 wall_up.position.y = 3.6;
 wall_down.position.y = -3.6;
 wall_left.position.x = -5.1;
 wall_right.position.x = 5.1;
 line.position.y = 0;
+ball.reciveShadow = true;
+
 
 //variables
 var velBall = .03;
@@ -47,113 +105,186 @@ scene.add( wall_down );
 scene.add( wall_left );
 scene.add( wall_right );
 scene.add( line );
+scene.add(light);
+scene.add(light1);
 
 const animate = function (ts) {
     requestAnimationFrame( animate );
 
+
     counter += 1;
-    // camera.rotation.x = 0.3;
     camera.position.z = 5;
-    // camera.position.y = -2;
-
-    var timeDelta = (ts - lastTs)/1000;
-    lastTs = ts;
-
-    ball.position.x += velBall;
-    ball.position.y += velBally;
-
-    var movSpeed = 5*timeDelta;
-
-    if(counter > 5000){
-      camera.rotation.z += 0.003;
-    }
-
-    if(ball.position.x - .1 <= -5 || ball.position.x + .1  >= 5){
-      velBall = -velBall;
-    }
-    if(ball.position.y - .1 <= -3.5 || ball.position.y + .1  >= 3.5){
-      velBally = -velBally;
-    }
-
-    if(player1.position.x+.15 >= ball.position.x-.1 && player1.position.x -.15 <= ball.position.x+.1 && player1.position.y+.5 >= ball.position.y && player1.position.y-.5 <= ball.position.y){
-      velBall = -velBall;
-    }
-
-    if(player2.position.x+.15 >= ball.position.x-.1 && player2.position.x -.15 <= ball.position.x+.1 && player2.position.y+.5 >= ball.position.y && player2.position.y-.5 <= ball.position.y){
-      velBall = -velBall;
-    }
-
-    if(input.isLeftPressed) {
-      if(player1.position.x - .15 < -5){
-        player1.position.x += 0;
-      }
-      else{
-        player1.position.x -= movSpeed;
-      }
-    }
-    
-    if(input.isRightPressed) {
-      if(player1.position.x + .15  > 0){
-        player1.position.x += 0;
-      }
-      else{
-        player1.position.x += movSpeed;
-      }
-      
-    }
-    if(input.isDownPressed) {
-      if(player1.position.y - .5 < -3.5){
-        player1.position.y += 0;
-      }
-      else{
-        player1.position.y -= movSpeed;
-      }
-    }
-    if(input.isUpPressed) {
-      if(player1.position.y + .5 > 3.5){
-        player1.position.y += 0;
-      }
-      else{
-        player1.position.y += movSpeed;
-      }
-    }
-
-    if(input.isLeftPressed1) {
-      if(player2.position.x - .15 < 0){
-        player2.position.x += 0;
-      }
-      else{
-        player2.position.x -= movSpeed;
-      }
-    }
-    
-    if(input.isRightPressed1) {
-      if(player2.position.x + .15  > 5){
-        player2.position.x += 0;
-      }
-      else{
-        player2.position.x += movSpeed;
-      }
-      
-    }
-    if(input.isDownPressed1) {
-      if(player2.position.y - .5 < -3.5){
-        player2.position.y += 0;
-      }
-      else{
-        player2.position.y -= movSpeed;
-      }
-    }
-    if(input.isUpPressed1) {
-      if(player2.position.y + .5 > 3.5){
-        player2.position.y += 0;
-      }
-      else{
-        player2.position.y += movSpeed;
-      }
-    }
-
     renderer.render( scene, camera );
+    
+    if (counter > 100){
+    
+      // camera.rotation.x = 0.3;
+      // camera.position.y = -2;
+
+      var timeDelta = (ts - lastTs)/1000;
+      lastTs = ts;
+
+      ball.position.x += velBall;
+      ball.position.y += velBally;
+
+      var movSpeed = 5*timeDelta;
+
+      if(counter > 5000){
+        camera.rotation.z += 0.003;
+      }
+
+      if(ball.position.x - .1 <= -5){
+        velBall = -velBall;
+        placar_1 += 1;
+        scene.remove(mesh);
+        loader.load('https://cdn.rawgit.com/mrdoob/three.js/master/examples/fonts/helvetiker_regular.typeface.json', function(font) {
+  
+          var text = new THREE.TextGeometry(`${placar_1} x ${placar_2}`, {
+            font: font,
+            size: 50,
+            height: 0,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 5,
+            bevelSize: 8,
+            bevelSegments: 5
+          });
+
+          var material = new THREE.MeshLambertMaterial({
+            color: 0xF3FFE2
+          });
+          mesh = new THREE.Mesh(text, material);
+          mesh.position.set(-.75, 2.5, -.3);
+          mesh.scale.multiplyScalar(0.01)
+          mesh.castShadow = true;
+          scene.add(mesh);
+        });
+        player1.position.x = -4;
+        player1.position.y = 0;
+        player2.position.x = 4;
+        player2.position.y = 0;
+        ball.position.x = 0;
+        ball.position.y = 0;
+        counter = 0;
+      }
+
+      if(ball.position.x + .1  >= 5){
+        velBall = -velBall;
+        placar_2 += 1;
+        scene.remove(mesh);
+        loader.load('https://cdn.rawgit.com/mrdoob/three.js/master/examples/fonts/helvetiker_regular.typeface.json', function(font) {
+  
+          var text = new THREE.TextGeometry(`${placar_1} x ${placar_2}`, {
+            font: font,
+            size: 50,
+            height: 0,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 5,
+            bevelSize: 8,
+            bevelSegments: 5
+          });
+
+          var material = new THREE.MeshLambertMaterial({
+            color: 0xF3FFE2
+          });
+          mesh = new THREE.Mesh(text, material);
+          mesh.position.set(-.75, 2.5, -.3);
+          mesh.scale.multiplyScalar(0.01)
+          mesh.castShadow = true;
+          scene.add(mesh);
+        });
+        player1.position.x = -4;
+        player1.position.y = 0;
+        player2.position.x = 4;
+        player2.position.y = 0;
+        ball.position.x = 0;
+        ball.position.y = 0;
+        counter = 0;
+      }
+
+      if(ball.position.y - .1 <= -3.5 || ball.position.y + .1  >= 3.5){
+        velBally = -velBally;
+      }
+
+      if(player1.position.x+.15 >= ball.position.x-.1 && player1.position.x -.15 <= ball.position.x+.1 && player1.position.y+.5 >= ball.position.y && player1.position.y-.5 <= ball.position.y){
+        velBall = -velBall;
+      }
+
+      if(player2.position.x+.15 >= ball.position.x-.1 && player2.position.x -.15 <= ball.position.x+.1 && player2.position.y+.5 >= ball.position.y && player2.position.y-.5 <= ball.position.y){
+        velBall = -velBall;
+      }
+
+      if(input.isLeftPressed) {
+        if(player1.position.x - .15 < -5){
+          player1.position.x += 0;
+        }
+        else{
+          player1.position.x -= movSpeed;
+        }
+      }
+      
+      if(input.isRightPressed) {
+        if(player1.position.x + .15  > 0){
+          player1.position.x += 0;
+        }
+        else{
+          player1.position.x += movSpeed;
+        }
+        
+      }
+      if(input.isDownPressed) {
+        if(player1.position.y - .5 < -3.5){
+          player1.position.y += 0;
+        }
+        else{
+          player1.position.y -= movSpeed;
+        }
+      }
+      if(input.isUpPressed) {
+        if(player1.position.y + .5 > 3.5){
+          player1.position.y += 0;
+        }
+        else{
+          player1.position.y += movSpeed;
+        }
+      }
+
+      if(input.isLeftPressed1) {
+        if(player2.position.x - .15 < 0){
+          player2.position.x += 0;
+        }
+        else{
+          player2.position.x -= movSpeed;
+        }
+      }
+      
+      if(input.isRightPressed1) {
+        if(player2.position.x + .15  > 5){
+          player2.position.x += 0;
+        }
+        else{
+          player2.position.x += movSpeed;
+        }
+        
+      }
+      if(input.isDownPressed1) {
+        if(player2.position.y - .5 < -3.5){
+          player2.position.y += 0;
+        }
+        else{
+          player2.position.y -= movSpeed;
+        }
+      }
+      if(input.isUpPressed1) {
+        if(player2.position.y + .5 > 3.5){
+          player2.position.y += 0;
+        }
+        else{
+          player2.position.y += movSpeed;
+        }
+      }
+    }
 };
 
 requestAnimationFrame(animate);
